5508d265bfdea9d9ab84e6b94738d941f6e958f1
YashK76 <yashk6690@gmail.com>
2024-06-28 19:29:03 +0530

    Add files via upload

---
+++ task.py
@@ -0,0 +1,20 @@
+#My_list 
+my_list =[10,15,20,25,30]
+my_list.append(35)
+my_list.remove(10)
+my_list[1] =(20)
+print("updated list:",my_list)
+
+#My_Dictionary
+my_dict = {'My_name': 'Yash' ,'age':21,'city':'mumbai'}
+my_dict['gender'] ='male'
+del my_dict['age']
+my_dict['city']='mumbai'
+print("updated Dictionary:",my_dict)
+
+#My_set
+my_set= {5,6,7,8,9}
+my_set.add(10)
+my_set.remove(5)
+my_set.discard(6)
+print("updated set:",my_set)
